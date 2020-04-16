ed3ad75f5e1491bd48f4c0cf890e665507c2f878
Saurabh <saurabhbharti9@gmail.com>
2020-04-17 04:32:41 +0530

    added configs and test react app

---
+++ src/server/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import App from "../components/App";
+import config from "./config";
+
+const server = express();
+server.use(express.static("dist"));
+
+server.get("/", (req, res) => {
+  const initialMarkup = ReactDOMServer.renderToString(<App />);
+
+  res.send(`
+    <html>
+      <head>
+        <title>Advanced React App</title>
+      </head>
+      <body>
+        <div id="mountNode">${initialMarkup}</div>
+        <script src="/main.js"></script>
+      </body>
+    </html>
+  `);
+});
+
+server.listen(config.port, () =>
+  console.log(`React Server is running on ${config.port}...`)
+);
